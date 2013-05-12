f2295bc908e514e0ac6eaed381082a098d596848
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2013-05-12 16:54:38 +0100

    TestingRelayCat: Remove unused imports

--- test/uk/co/harcourtprogramming/internetrelaycats/TestingRelayCat.java
+++ test/uk/co/harcourtprogramming/internetrelaycats/TestingRelayCat.java
@@ -1,7 +1,5 @@
 package uk.co.harcourtprogramming.internetrelaycats;
 
-import java.io.IOException;
-import java.net.UnknownHostException;
 import java.util.LinkedList;
 import java.util.Queue;
 import uk.co.harcourtprogramming.mewler.LocalBufferedSocket;
